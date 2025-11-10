c1375dcd37f23815642d2c70b23431d560c04dca
Charles Ross <charles.ross.mc40@gmail.com>
2025-11-09 19:18:35 -0800

    ready

--- android/app/src/main/java/com/stubly/app/MainActivity.java
+++ android/app/src/main/java/com/stubly/app/MainActivity.java
@@ -1,11 +1,13 @@
 package com.stubly.app;
 
 import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         registerPlugin(SystemBarsPlugin.class);
         registerPlugin(StorageManagerPlugin.class);
         super.onCreate(savedInstanceState);
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.stubly.app',
-  appName: 'Stubly.io',
+  appName: 'Stubly',
   webDir: 'build'
 };
 
